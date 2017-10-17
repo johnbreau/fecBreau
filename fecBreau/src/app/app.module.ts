432109c179b35d5884fd190be86ef786e075d58d
johnbreau <johnbreau@github.com>
2017-10-16 22:09:47 -0500

    initial commit

--- fecBreau/src/app/app.module.ts
+++ fecBreau/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form-component/form.component';
@@ -10,7 +11,8 @@ import { FormComponent } from './form-component/form.component';
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fecBreau/src/app/form-component/form.component.ts
+++ fecBreau/src/app/form-component/form.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Set } from '../setInterface';
 
 @Component({
   selector: 'app-form-component',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  public setForm: FormGroup;
 
-  constructor() { }
+  constructor ( formBuilder: FormBuilder ) {
+    this.setForm = formBuilder.group({
+      setName: ['', Validators.minLength(2)],
+      setNumber: ['', Validators.minLength(2)],
+      setPieces: '',
+      setYear: '',
+      setTheme: '',
+    });
+   }
 
   ngOnInit() {
   }
 
 }
+
