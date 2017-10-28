aed295dca07cdf9d1af365ab7dfe1f5c203508b8
johnbreau <johnbreau@github.com>
2017-10-28 08:06:27 -0500

    adding db

--- fecBreau/src/app/form-component/form.component.ts
+++ fecBreau/src/app/form-component/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {AngularIndexedDB} from 'angular2-indexeddb';
 import { Set } from '../setInterface';
 
 @Component({
@@ -9,6 +10,7 @@ import { Set } from '../setInterface';
 })
 export class FormComponent implements OnInit {
   public setForm: FormGroup;
+  public db: AngularIndexedDB;
 
   constructor (private formBuilder: FormBuilder ) {
    }
@@ -28,7 +30,25 @@ export class FormComponent implements OnInit {
       setTheme: '',
       disabled: [false]
     });
+    // Open indexDB Database...
+    this.db = new AngularIndexedDB('setDb', 1);
+
+    this.db.openDatabase(1, (evt) => {
+      const objectStore = evt.currentTarget.result.createObjectStore(
+          'id', { keyPath: 'id', autoIncrement: true },
+          'setName', { keyPath: 'setName', autoIncrement: false },
+          'setNnumber', { keyPath: 'setNnumber', autoIncrement: false },
+          'setPieces', { keyPath: 'setPieces', autoIncrement: false },
+          'setYear', { keyPath: 'setYear', autoIncrement: false },
+          'setTheme', { keyPath: 'setTheme', autoIncrement: false }
+        );
+      objectStore.createIndex('id', 'id', { unique: true });
+      objectStore.createIndex('setName', 'setName', { unique: false });
+      objectStore.createIndex('setNnumber', 'setNnumber', { unique: true });
+      objectStore.createIndex('setPieces', 'setPieces', { unique: false });
+      objectStore.createIndex('setYear', 'setYear', { unique: false });
+      objectStore.createIndex('setTheme', 'setTheme', { unique: false });
+    });
+   console.log('anything?', this.db); 
   }
-
 }
-
