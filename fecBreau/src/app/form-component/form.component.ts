f79eb13f557719dd923e481765c50babb57bbd1f
John Breau <john.breau@vml.com>
2017-11-13 11:27:26 -0600

    db and form working - not able to submit object yet

--- fecBreau/src/app/form-component/form.component.ts
+++ fecBreau/src/app/form-component/form.component.ts
@@ -1,57 +1,70 @@
-import { Component, OnInit, EventEmitter, Output, OnChanges } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {AngularIndexedDB} from 'angular2-indexeddb';
 import { Set } from '../setInterface';
-import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-form-component',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit, OnChanges {
+export class FormComponent implements OnInit {
+  public setForm: FormGroup;
   public DBOpenRequest: any;
   public db: any;
-  setForm: FormGroup;
 
   constructor (private formBuilder: FormBuilder ) {
    }
 
   ngOnInit() {
     this.setForm = this.formBuilder.group({
-      setName: ['', Validators.compose([Validators.required])],
-      setNumber: ['', Validators.compose([Validators.required])],
+      setName: [
+        '',
+        Validators.compose([Validators.required])
+      ],
+      setNumber: [
+        '',
+        Validators.compose([Validators.required])
+      ],
       setPieces: '',
       setYear: '',
       setTheme: '',
       disabled: [false]
     });
-    // Open indexDB Database...
-    this.DBOpenRequest = window.indexedDB.open('setDb', 1);
+    const db = new AngularIndexedDB('myDb', 1);
 
-    this.DBOpenRequest.onerror = function(event) {
-      console.log('Loading...');
-    };
+    db.openDatabase(1, (evt) => {
+      // const objectStore = evt.currentTarget.result.createObjectStore(
+      //     'people', { keyPath: "id", autoIncrement: true });
 
-    this.DBOpenRequest.onsuccess = function(event) {
-      console.log('Database initialised');
-    };
+      // objectStore.createIndex("name", "name", { unique: false });
+      // objectStore.createIndex("email", "email", { unique: true });
+
+      const objectStore = evt.currentTarget.result.createObjectStore(
+          'setDatabase', { keyPath: 'setName', autoIncrement: true });
+
+      objectStore.createIndex('setName', 'setName', { unique: false });
+      objectStore.createIndex('setNnumber', 'setNnumber', { unique: true });
+      objectStore.createIndex('setPieces', 'setPieces', { unique: false });
+      objectStore.createIndex('setYear', 'setYear', { unique: false });
+      objectStore.createIndex('setTheme', 'setTheme', { unique: false });
+
+  });
+
+  db.getByKey('people', 1).then((person) => {
+    console.log(person);
+    }, (error) => {
+        console.log(error);
+  });
+
   }
-
-  ngOnChanges() {
-    this.db = this.DBOpenRequest.result;
-
-    const objectStore = this.db.createObjectStore('setDatabase', { keyPath: 'setName' });
-          objectStore.createIndex('setName', 'setName', { unique: false });
-          objectStore.createIndex('setNnumber', 'setNnumber', { unique: true });
-          objectStore.createIndex('setPieces', 'setPieces', { unique: false });
-          objectStore.createIndex('setYear', 'setYear', { unique: false });
-          objectStore.createIndex('setTheme', 'setTheme', { unique: false });
-       console.log('anything?', objectStore);
-  }
-
-  getSet() {
-    window.alert('getSet!');
-  }
-
 }
 
+// const objectStore = this.db.createObjectStore('setDatabase', { keyPath: 'setName' });
+//       objectStore.createIndex('setName', 'setName', { unique: false });
+//       objectStore.createIndex('setNnumber', 'setNnumber', { unique: true });
+//       objectStore.createIndex('setPieces', 'setPieces', { unique: false });
+//       objectStore.createIndex('setYear', 'setYear', { unique: false });
+//       objectStore.createIndex('setTheme', 'setTheme', { unique: false });
+//     console.log('anything?', objectStore);
+// });
